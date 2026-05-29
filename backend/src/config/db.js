2ffa444665af03ca53f08932bc07a51218fde844
ankit8895 <ankit9185731@gmail.com>
2026-05-30 01:54:08 +0530

    backend: fixing bug - mongoDB refuse connection on production

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,12 +1,38 @@
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
+// import { ENV } from "./env.js";
+
+// export const connectDB = async () => {
+//   try {
+//     const conn = await mongoose.connect(ENV.MONGODB_URI);
+//     console.log("MongoDB connected successfully:", conn.connection.host);
+//   } catch (error) {
+//     console.error("Error connecting to database:", error);
+//     process.exit(1);
+//   }
+// };
+
+import { connect } from "mongoose";
 import { ENV } from "./env.js";
 
-export const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(ENV.MONGODB_URI);
-    console.log("MongoDB connected successfully:", conn.connection.host);
-  } catch (error) {
-    console.error("Error connecting to database:", error);
+const connectDB = async () => {
+  const uri = ENV.MONGODB_URI;
+
+  console.log(
+    "MONGODB_URI prefix:",
+    uri ? uri.split("@")[1]?.slice(0, 40) + "..." : "undefined",
+  );
+
+  if (!uri) {
+    console.error("MONGODB_URI is not defined");
     process.exit(1);
   }
+
+  await connect(uri, {
+    serverSelectionTimeoutMS: 10000,
+    socketTimeoutMS: 20000,
+  });
+
+  console.log("MongoDB connected successfully");
 };
+
+export default connectDB;
